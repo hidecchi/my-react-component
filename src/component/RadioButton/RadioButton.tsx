7c3598735e3bbcbcaa75682484f0104b346ac49f
Okawa <hidehide21@gmail.com>
2025-01-13 20:49:47 +0900

    format

--- src/component/RadioButton/RadioButton.tsx
+++ src/component/RadioButton/RadioButton.tsx
@@ -5,16 +5,16 @@ import type { InputHTMLAttributes } from "react";
 import styles from "./RadioButton.module.scss";
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-	label: string;
+  label: string;
 }
 
 export const RadioButton = (props: Props) => {
-	const { label, className, ...otherProps } = props;
-	return (
-		<label className={clsx(styles.component, className)}>
-			<input {...otherProps} className={styles.input} type="radio" />
-			<span className={styles.alternativeInput} />
-			<span className={styles.labelText}>{label}</span>
-		</label>
-	);
+  const { label, className, ...otherProps } = props;
+  return (
+    <label className={clsx(styles.component, className)}>
+      <input {...otherProps} className={styles.input} type="radio" />
+      <span className={styles.alternativeInput} />
+      <span className={styles.labelText}>{label}</span>
+    </label>
+  );
 };
